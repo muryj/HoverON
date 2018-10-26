27f4a8ff9cfe7134e44eaceaa3e2f72fe4c08a90
muryj <jekntr@gmail.com>
2018-10-26 22:48:38 +0300

    Applied new styles to main page

---
+++ script.js
@@ -0,0 +1,34 @@
+$(function() {
+	var link = $('.m-menu-link');
+	var close = $('.close-menu');
+	var menu = $('.m-menu');
+	link.on('click', function(event) {
+		event.preventDefault();
+		menu.toggleClass('m-menu_activ');
+	});
+	close.on('click', function(event) {
+		event.preventDefault();
+		menu.toggleClass('m-menu_activ');
+	});
+	var arrow = $('.downArrow a');
+	arrow.on('click',function (event) { 
+		event.preventDefault();
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$('html, body').animate( { scrollTop: destination }, 1100 );
+	  });
+	var button =$('.features');
+	button.on('click',function (event) { 
+		event.preventDefault();
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$('html, body').animate( { scrollTop: destination }, 1100 );
+	});
+	var sbutton =$('.exploreF');
+	sbutton.on('click',function (event) { 
+		event.preventDefault();
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$('html, body').animate( { scrollTop: destination }, 1100 );
+	});
+});
