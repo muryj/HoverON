891d987d4c83e4dde637c11848f7721009240324
muryj <jekntr@gmail.com>
2018-10-27 12:11:18 +0300

    Search form added on main page

--- script.js
+++ script.js
@@ -40,4 +40,24 @@ $(function() {
 	document.querySelector('.md-close').onclick = () => {
 	  modalWindow.classList.remove('md-show')
 	}
+	/* searchform */
+	$("#search").focus(function() {
+		$(".search-box").addClass("border-searching");
+		$(".search-icon").addClass("si-rotate");
+	});
+	$("#search").blur(function() {
+		$(".search-box").removeClass("border-searching");
+		$(".search-icon").removeClass("si-rotate");
+	});
+	$("#search").keyup(function() {
+			if($(this).val().length > 0) {
+				$(".go-icon").addClass("go-in");
+			}
+			else {
+				$(".go-icon").removeClass("go-in");
+			}
+	});
+	$(".go-icon").click(function(){
+		$(".search-form").submit();
+	});
 });
